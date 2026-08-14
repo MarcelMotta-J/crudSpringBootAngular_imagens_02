09e51b6980f89657c293e83b6e935a448e03ba59
MarcelMotta-J <marceldsmotta@gmail.com>
2026-08-14 18:27:33 -0300

    feat: add frontend user search

--- frontend/crud_springb_angular/src/app/user-list/user-list.ts
+++ frontend/crud_springb_angular/src/app/user-list/user-list.ts
@@ -8,43 +8,81 @@ import { CommonModule } from '@angular/common';
 
 //import { NgIf } from '@angular/common'; 
 
+import { FormsModule } from '@angular/forms';
+
+
+
 @Component({
-  selector: 'app-user-list',  
-  imports: [CommonModule, RouterModule], // 1h28min20seg
+  selector: 'app-user-list',
+  imports: [
+    CommonModule,
+    RouterModule,
+    FormsModule
+  ],
   providers: [UserService],
   templateUrl: './user-list.html',
   styleUrl: './user-list.css',
-})
-export class UserList implements OnInit{
 
-  
+})
+export class UserList implements OnInit {
+
+
 
   users: User[] = [];
 
-  constructor(private userService : UserService, private router : Router){}
+  filteredUsers: User[] = [];
+  searchTerm = '';
+
+  constructor(private userService: UserService, private router: Router) { }
 
   ngOnInit(): void {
     this.getUsers();
   }
 
-  getUsers(){
-    this.userService.getUsers().subscribe((data : User[])=> {
+  getUsers() {
+    this.userService.getUsers().subscribe((data: User[]) => {
       this.users = data; // store the fetched data
-      
+      this.filteredUsers = data;
+
       console.log(this.users)
     });
   }
 
-  deleteUser(id: any){
-    this.userService.deleteUser(id).subscribe(()=>{
+  deleteUser(id: any) {
+    this.userService.deleteUser(id).subscribe(() => {
       this.getUsers(); // list refresh after delete
     });
 
   }
 
-  editUser(id: any){
+  editUser(id: any) {
     this.router.navigate([`/users/edit/${id}`]);
 
+  }
+
+  searchUsers(): void {
+
+    const term = this.searchTerm
+      .trim()
+      .toLowerCase();
+
+    if (!term) {
+      this.filteredUsers = this.users;
+      return;
+    }
+
+    this.filteredUsers = this.users.filter(user => {
+
+      const firstname = user.firstname?.toLowerCase() ?? '';
+      const lastname = user.lastname?.toLowerCase() ?? '';
+      const email = user.email?.toLowerCase() ?? '';
+
+      return (
+        firstname.includes(term) ||
+        lastname.includes(term) ||
+        email.includes(term)
+      );
+    });
   }
 
 
